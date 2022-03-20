8e1693c5e771c6a2dd7a69ff7d5ac7d3766deb9e
jeong moon <ijhghji@gmail.com>
2022-03-21 00:33:56 +0900

    수정

--- test.js
+++ test.js
@@ -7,3 +7,14 @@ console.log("b의 값은",b);
 
 b ="문자";
 console.log("변화된 b의 값은",b);
+
+
+fn_axios(async(B_no)=>{
+    const status = "true";
+    await axios.get('/board').then((res)=>{
+        console.log(res.data);
+    }).catch((e)=>{
+        console.log(e)
+    })
+    console.log("")
+})
